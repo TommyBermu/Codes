1aec4528ee24022db1b23f7ee80d566cafd9091b
Tomas Bermudez <bermudeztomas06@gmail.com>
2024-03-18 23:38:57 -0500

    finish :DD

--- Java/Universidad/Triki.java
+++ Java/Universidad/Triki.java
@@ -1,6 +1,4 @@
 package Universidad;
-//import java.util.Scanner;
-
 import java.util.Scanner;
 
 class Triki {
@@ -12,39 +10,30 @@ class Triki {
             }
         }
     }
-    
+
     public void marcarCasilla(String simbolo, int fila, int columna) {
         triki[fila][columna] = simbolo;
     }
 
-    String verificarGanador(){
-        // verificar filas
-        if (triki[0][0] == triki[0][1] && triki[0][1] == triki[0][2]){
+    String verificarGanador() {
+        // Verificar filas y columnas
+        for (int i = 0; i < 3; i++) {
+            if (triki[i][0].equals(triki[i][1]) && triki[i][1].equals(triki[i][2]) && !triki[i][0].equals("a")) {
+                return triki[i][0];
+            }
+            if (triki[0][i].equals(triki[1][i]) && triki[1][i].equals(triki[2][i]) && !triki[0][i].equals("a")) {
+                return triki[0][i];
+            }
+        }
+        
+        // Verificar diagonales
+        if (triki[0][0].equals(triki[1][1]) && triki[1][1].equals(triki[2][2]) && !triki[0][0].equals("a")) {
             return triki[0][0];
         }
-        if (triki[1][0] == triki[1][1] && triki[1][1] == triki[1][2]){
-            return triki[1][0];
-        }
-        if (triki[2][0] == triki[2][1] && triki[2][1] == triki[2][2]){
-            return triki[2][0];
-        }
-        // Verificar columnas
-        if (triki[0][0] == triki[1][0] && triki[1][0] == triki[2][0]){
-            return triki[0][0];
-        }
-        if (triki[0][1] == triki[1][1] && triki[1][1] == triki[2][1]){
-            return triki[0][1];
-        }
-        if (triki[0][2] == triki[1][2] && triki[1][2] == triki[2][2]){
+        if (triki[0][2].equals(triki[1][1]) && triki[1][1].equals(triki[2][0]) && !triki[0][2].equals("a")) {
             return triki[0][2];
         }
-        //verificar diagonales
-        if (triki[0][0] == triki[1][1] && triki[1][1] == triki[2][2]){
-            return triki[0][0];
-        }
-        if (triki[0][2] == triki[1][1] && triki[1][1] == triki[2][0]){
-            return triki[0][2];
-        }
+    
         return "a";
     }
 
@@ -55,7 +44,7 @@ class Triki {
     void imprimirTablero(){
         for (byte i = 0; i < triki.length; i++) {
             for (byte j = 0; j < triki[i].length; j++) {
-                System.out.print(triki[i][j] + " ");
+                System.out.print(triki[i][j]);
             }
             System.out.println();
         }
@@ -66,10 +55,31 @@ class Solution0 {
     public static void main(String[] args) {
         Triki mat = new Triki();
         Scanner sc = new Scanner(System.in);
-        while (true){
+        mat.triki();
+        String casilla;
+        int fila, columna;
+        while (mat.verificarGanador().equals("a")){
+            casilla = sc.nextLine();
+            while (!casilla.equals("O") && !casilla.equals("X")){
+                    System.out.println("SNA");
+                    casilla = sc.nextLine();
+            }
+            System.out.println("SA");
+
+            fila = sc.nextInt();
+            columna = sc.nextInt();
+            while (fila < 0 || fila > 2 || columna < 0 || columna > 2 || !mat.verificarCasilla(fila, columna).equals("a")) {
+                System.out.println("CNA");
+                fila = sc.nextInt();
+                columna = sc.nextInt();
+            }
+            if(sc.hasNextLine())sc.nextLine();
+            System.out.println("CA");
             
-            mat.marcarCasilla(sc.nextLine(), sc.nextInt(), sc.nextInt());
+            mat.marcarCasilla(casilla, fila, columna);
         }
+        mat.imprimirTablero();
+        System.out.println("El simbolo ganador fue: " + mat.verificarGanador());
         sc.close();
     }
 }
