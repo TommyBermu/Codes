d479ba623079755d791cded8cf4fdda0c3ea0aba
Tomas Bermudez <bermudeztomas06@gmail.com>
2024-08-09 23:48:39 -0500

    Taller terminado :D

--- Java/src/Taller_2/Fraccion.java
+++ Java/src/Taller_2/Fraccion.java
@@ -1,55 +1,127 @@
 package Taller_2;
-public class Fraccion{
-    private int num_crudo, den_crudo, num, den, coc, res;
+
+import java.util.ArrayList;
+
+public class Fraccion implements Comparable<Fraccion> {
+    long num_crudo, den_crudo, num, den, coc, res;
     private String signo;
 
-    public Fraccion(int num, int den) {
-        this.num_crudo = num;
+    public Fraccion(long num, long den) {
+        Asignar(num, den);
+    }
+
+    private void Asignar(long num, long den) {
+        if (den == 0)
+            throw new IllegalArgumentException("El denominador no puede ser 0");
         this.den_crudo = den;
-        this.signo = this.num_crudo>=0 ^ this.den_crudo>=0 ? "-" : "";
-        this.num = this.num_crudo < 0 ? -this.num_crudo : this.num_crudo;
-        this.den = this.den_crudo < 0 ? -this.den_crudo : this.den_crudo;
-        this.coc = this.num/this.den;
-        this.res = this.num%this.den;
+        this.num_crudo = num;
+        this.num = num < 0 ? -num : num;
+        this.den = den < 0 ? -den : den;
+        this.signo = num >= 0 ^ den >= 0 ? "-" : "";
+        this.coc = this.num / this.den;
+        this.res = this.num % this.den;
+    }
+
+    String Simplificado() {
+        return "" + signo + (num != den && den != 1 ? num + "/" + den : (den != 0 ? num / den : num));
+    }
+
+    String Propio() {
+        return "" + signo + num + "/" + den;
+    }
+
+    String Mixto() {
+        return "" + signo + (coc != 0 ? coc : "") + (coc != 0 && res != 0 ? " " : "") + (res != 0 ? +res + "/" + den : "");
+    }
+
+    void Suma(Fraccion target) {
+        simplificar();
+        target.simplificar();
+        Asignar(num_crudo * target.den_crudo + den_crudo * target.num_crudo, den_crudo * target.den_crudo);
+    }
+
+    static Fraccion Suma(Fraccion first, Fraccion second) {
+        return new Fraccion(first.num_crudo * second.den_crudo + first.den_crudo * second.num_crudo, first.den_crudo * second.den_crudo);
+    }
+
+    void Resta(Fraccion target) {
+        simplificar();
+        target.simplificar();
+        Asignar(num_crudo * target.den_crudo - den_crudo * target.num_crudo, den_crudo * target.den_crudo);
+    }
+
+    static Fraccion Resta(Fraccion first, Fraccion second) {
+        return new Fraccion(first.num_crudo * second.den_crudo - first.den_crudo * second.num_crudo, first.den_crudo * second.den_crudo);
+    }
+
+    void Multiplicacion(Fraccion target) {
+        simplificar();
+        target.simplificar();
+        Asignar(num_crudo * target.num_crudo, den_crudo * target.den_crudo);
+    }
+
+    static Fraccion Multiplicacion(Fraccion first, Fraccion second) {
+        return new Fraccion(first.num_crudo * second.num_crudo, first.den_crudo * second.den_crudo);
+    }
+
+    void Division(Fraccion target) {
+        simplificar();
+        target.simplificar();
+        Asignar(num_crudo * target.den_crudo, den_crudo * target.num_crudo);
+    }
+
+    static Fraccion Division(Fraccion first, Fraccion second) {
+        return new Fraccion(first.num_crudo * second.den_crudo, first.den_crudo * second.num_crudo);
+    }
+
+    public double getValue() {
+        return (double) num_crudo / (double) den_crudo;
+    }
+
+    boolean esMayor(Fraccion target) {
+        return this.getValue() > target.getValue();
+    }
+
+    boolean esMenor(Fraccion target) {
+        return this.getValue() < target.getValue();
+    }
+
+    boolean esIgual(Fraccion target) {
+        return this.getValue() == target.getValue();
     }
     
-    String Propio(){
-        return "" + this.signo + this.num +"/"+ this.den;
+    public void simplificar() {
+        long div = GCD(den_crudo, res);
+        Asignar(num_crudo / div, den_crudo / div);
     }
 
-    String Mixto(){
-        return "" + this.signo + (this.coc != 0 ? this.coc : "") + (this.coc != 0 && this.res != 0 ? " " : "") + (this.res != 0 ? + this.res +"/"+ this.den : "");
+    long GCD(long a, long b) {
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
+        return GCD(b, a % b);
     }
 
-    Fraccion Suma(Fraccion target){
-        return new Fraccion(this.num_crudo * target.den_crudo + this.den_crudo * target.num_crudo, this.den_crudo * target.den_crudo);
+    @Override
+    public int compareTo(Fraccion fr) {
+        if (esMayor(fr))
+            return 1;
+        if (esMenor(fr))
+            return -1;
+        return 0;
     }
 
-    Fraccion Resta(Fraccion target){
-        return new Fraccion(this.num_crudo * target.den_crudo - this.den_crudo * target.num_crudo, this.den_crudo * target.den_crudo);
-    }
-    
-    Fraccion Multiplicacion(Fraccion target){
-        return new Fraccion(this.num_crudo * target.num_crudo, this.den_crudo * target.den_crudo);
-    }
-    
-    Fraccion Division(Fraccion target){
-        return new Fraccion(this.num_crudo * target.den_crudo, this.den_crudo * target.num_crudo);
+    public String toString() {
+        return Simplificado();
     }
 
-    boolean esMayor(Fraccion target){
-        return (double)(this.num_crudo)/(double)(this.den_crudo) > (double)(target.num_crudo)/(double)(target.den_crudo);
-    }
-
-    boolean esMenor(Fraccion target){
-        return !esMayor(target);
-    }
-
-    double Diferencia(Fraccion sustraendo){
-        return (double)(this.num_crudo * sustraendo.den_crudo - this.den_crudo * sustraendo.num_crudo) / (double)(this.den_crudo * sustraendo.den_crudo);
-    }
-
-    Fraccion simplificar(){
-        return new Fraccion(num, den);
+    public static Fraccion sumatoria(ArrayList<Fraccion> lista) {
+        Fraccion sum = new Fraccion(0, 1);
+        for (Fraccion fraccion : lista) {
+            sum.Suma(fraccion);
+            sum.simplificar();
+        }
+        return sum;
     }
 }
--- Java/src/Taller_2/Solution.java
+++ Java/src/Taller_2/Solution.java
@@ -1,112 +1,124 @@
 package Taller_2;
+
 import java.util.*;
 import java.lang.Math;
 
 public class Solution {
     public static void main(String[] args) {
+        // Fracciones4();
+        // ArregloDeFracciones();
+        // DivisionDeFracciones();
+        // FraccionMasCercano();
+        // Fracciones5();
+    }
+
+    public static void Fracciones4() {
+
         // Fracciones 4
-        
-        /*
 
         Scanner sc = new Scanner(System.in);
         Fraccion fraccion = new Fraccion(sc.nextInt(), sc.nextInt());
-        System.out.println(fraccion.Propio() +" "+ fraccion.Mixto());
+        System.out.println(fraccion.Propio() + " " + fraccion.Mixto());
         sc.close();
+    }
 
-        */
+    public static void ArregloDeFracciones() {
 
-        // Arreglo de Fracciones 
-
-        /*
+        // Arreglo de Fracciones
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         String propios[] = new String[N];
         String mixtos[] = new String[N];
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             Fraccion frac = new Fraccion(sc.nextInt(), sc.nextInt());
             propios[i] = frac.Propio();
             mixtos[i] = frac.Mixto();
         }
         String option = sc.next();
-        if (option.equals("p")){
-            for(int i = 0; i < N; i++){
+        if (option.equals("p")) {
+            for (int i = 0; i < N; i++) {
                 System.out.print(propios[i] + " ");
             }
         } else if (option.equals("m")) {
-            for(int i = 0; i < N; i++){
+            for (int i = 0; i < N; i++) {
                 System.out.print(mixtos[i] + " ");
             }
         } else if (option.equals("a")) {
-            for(int i = 0; i < N; i++){
+            for (int i = 0; i < N; i++) {
                 System.out.print(propios[i] + " " + mixtos[i] + (mixtos[i].equals("1") || mixtos[i].equals("-1") ? "" : " "));
             }
         }
         sc.close();
-        
-        */
-        
-        // División de Fracciones 
+    }
 
-        /*
+    public static void DivisionDeFracciones() {
+
+        // División de Fracciones
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         String resultado = "";
-        List <Fraccion> lista = new ArrayList<Fraccion>();
-        for(int i = 0; i < N; i++){
+        List<Fraccion> lista = new ArrayList<Fraccion>();
+        for (int i = 0; i < N; i++) {
             lista.add(new Fraccion(sc.nextInt(), sc.nextInt()));
         }
 
-        for (int i = 0; i < N-1; i++) {
-            System.out.println(resultado);
-            resultado += lista.get(i).Division(lista.get(i+1)).Mixto() + " ";
+        for (int i = 0; i < N - 1; i++) {
+            lista.set(i, Fraccion.Division(lista.get(i), lista.get(i + 1)));
+            resultado += lista.get(i).Mixto() + " ";
         }
         System.out.println(resultado);
         sc.close();
+    }
 
-        */
+    public static void FraccionMasCercano() {
 
         // Fracción más Cercano
 
-        /* 
-
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        List <Fraccion> lista = new ArrayList<Fraccion>();
-        for(int i = 0; i < N; i++){
+        List<Fraccion> lista = new ArrayList<Fraccion>();
+
+        for (int i = 0; i < N; i++) {
             lista.add(new Fraccion(sc.nextInt(), sc.nextInt()));
         }
+
         Fraccion target = new Fraccion(sc.nextInt(), sc.nextInt());
         int posicion = 0;
-        double menor = Math.abs(lista.get(0).Diferencia(target));
+        double menor = Math.abs(lista.get(0).getValue() - target.getValue());
+
         for (int i = 1; i < N; i++) {
-            if (Math.abs(lista.get(i).Diferencia(target)) < menor) {
-                menor = Math.abs(lista.get(i).Diferencia(target));
+            if (Math.abs(lista.get(i).getValue() - target.getValue()) < menor) {
+                menor = Math.abs(lista.get(i).getValue() - target.getValue());
                 posicion = i;
             }
         }
-        System.out.println(lista.get(posicion).Resta(target).Mixto());
+        System.out.println(target.den_crudo);
+        System.out.println(Fraccion.Resta(lista.get(posicion), target).Propio());
         System.out.println(lista.get(posicion).Mixto());
         sc.close();
 
-        */
+    }
+
+    public static void Fracciones5() {
 
         // Fracciones 5
 
-        /*
-
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        List <Fraccion> lista = new ArrayList<Fraccion>();
-        for(int i = 0; i < N; i++){
+        ArrayList<Fraccion> lista = new ArrayList<Fraccion>();
+        for (int i = 0; i < N; i++) {
             lista.add(new Fraccion(sc.nextInt(), sc.nextInt()));
         }
+        sc.close();
+        for (Fraccion fraccion : lista) {
+            fraccion.simplificar();
+        }
+        Collections.sort(lista);
         for (Fraccion fraccion : lista) {
             System.out.println(fraccion);
         }
-        sc.close();
-
-        */
+        System.out.println(Fraccion.sumatoria(lista));
     }
 }
